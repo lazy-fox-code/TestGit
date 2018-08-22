cc84dd806aaf598fae3872b4a6cb472f5e32126d
Lazy Fox <lazy-fox@yandex.ru>
2018-08-22 20:31:34 +0300

    2

--- app/src/main/java/ru/rm/testgit/MainActivity.java
+++ app/src/main/java/ru/rm/testgit/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+//123
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
